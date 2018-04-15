414b75a6c64fd9ecad3ec98e45d1f5c42c367128
tarik.djurdjevic <tarik.djurdjevic@gmail.com>
2018-04-15 16:15:19 +0200

    Adding file structure to backend and using mongoose instead of mongojs

--- client/builds/app.js
+++ client/builds/app.js
@@ -1,16 +1,4 @@
 var myApp = angular.module('myApp',[]);
-angular.module('myApp').directive("profileBox", function() {
-    return {
-        restrict : "E",
-        templateUrl : "/directives/profile-box.html"
-    };
-});
-angular.module('myApp').directive("tweetList", function() {
-    return {
-        restrict : "E",
-        templateUrl : "/directives/tweets.html"
-    };
-});
 angular.module('myApp').controller('TweetController', ['$scope','$http',function($scope,$http) {
   var userId="5ab14023a976e18bce0b912a";//hard coded because we don't have authentication
   
@@ -54,3 +42,15 @@ var User={
 angular.module('myApp').controller('UserController', ['$scope',function($scope) {
   $scope.user=User;
 }]);
+angular.module('myApp').directive("profileBox", function() {
+    return {
+        restrict : "E",
+        templateUrl : "/directives/profile-box.html"
+    };
+});
+angular.module('myApp').directive("tweetList", function() {
+    return {
+        restrict : "E",
+        templateUrl : "/directives/tweets.html"
+    };
+});
